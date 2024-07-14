0a3da81a76f0e57873060e25d25094d299b6c4af
webhaodai <buatwebsite011@gmail.com>
2024-07-14 16:33:08 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Toggle & Responsive
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// Reset Before unload form
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
